5af79c4744ca0d4150b2022685719a07d2676b47
amin <aminibrahim25@outlook.com>
2024-03-20 00:02:06 -0400

    first commit

--- app.js
+++ app.js
@@ -8,94 +8,115 @@
 // check if all values are truthy
 // turn All Elements in an Array into 0
 
-// 1.Find the smallest number
-function smallestNum(array) {
-    let smallestNum;
-    for (let i = 0; i < array.length; ++i) {
-      if (smallestNum === undefined) {
-        smallestNum = array[i];
-      } else if (smallestNum > array[i]) {
-        smallestNum = array[i];
-      }
+// 1.find the smallest number
+ function smallestNumber(array){
+  let smallestnum
+  for (let i = 0; i < array.length; i++) {
+    if(smallestnum===undefined){
+      smallestnum=array[i]
     }
-    return smallestNum;
-  }
-  console.log(smallestNum([2, -3, 4, 6, -2, 34]));
-  
-  // 2.Find the biggest number
-  function biggestNum(array) {
-    let biggestNum;
-    for (let i = 0; i < array.length; ++i) {
-      if (biggestNum === undefined) {
-        biggestNum = array[i];
-      } else if (biggestNum < array[i]) {
-        biggestNum = array[i];
-      }
+    else if(smallestnum>array[i]){
+      smallestnum=array[i]
     }
-    return biggestNum;
   }
-  console.log(biggestNum([2, 4, 5, 344, 4234, 4]));
-  
-  // 3.Count evens
-  
-  function countEvens(array) {
-    let count = 0;
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] % 2 === 0) {
-        count += 1;
-      }
+  return smallestnum
+ }
+ console.log(smallestNumber([2,4,5]))
+
+//  2.find the biggest number
+function biggestnum(array){
+  let biggestnum
+  for (let i = 0; i < array.length; i++) {
+    if(biggestnum===undefined){
+      biggestnum=array[i]
     }
-    return count;
-  }
-  console.log(countEvens([2, 4, 5, 6, 20, 50]));
-  
-  // 4.Count odds
-  
-  function countOdds(array) {
-    let count = 0;
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] % 2 !== 0) {
-        count += 1;
-      }
+    else if (biggestnum<array[i]){
+      biggestnum=array[i]
     }
-    return count;
   }
-  console.log(countOdds([3, 5, 6, 3, 5]));
-  
-  // 5.Remove duplicates from an array
-  
-  function removeDu(array) {
-    let newarr = [];
-    for (let i = 0; i < array.length; i++) {
-      if (!newarr.includes(array[i])) {
-        newarr.push(array[i])
-      }
+  return biggestnum
+}
+console.log(biggestnum([2,4,3453,54353,54,453]))
+
+// 3.count evens
+function evencount(array){
+  let count=0
+  for (let i = 0; i < array.length; i++) {
+   if(array[i]%2===0){
+    count+=1
+   }
+  }
+  return count
+}
+console.log(evencount([2,4,5,1,4,2]))
+
+// 4.count odds
+function oddcount(array){
+  let count=0
+  for (let i = 0; i < array.length; i++) {
+    if(array[i]%2!==0){
+      count+=1
     }
-    return newarr
   }
-  console.log(removeDu([2,5,6,7,5,5,2]))
-   
-  // 6.how much is true
-  
-  function truecount(array){
-    let count=0
-    for (let i = 0; i < array.length; i++) {
-     if(array[i]===true)
-     count+=1
-      
+  return count
+}
+console.log(oddcount([2,4,5,6,3]))
+
+// 5. remove duplicates from an array
+function removeDu(array){
+  let newarr=[]
+  for (let i = 0; i < array.length; i++) {
+   if(!newarr.includes(array[i])){
+       newarr.push(array[i])
+   }
+  }
+  return newarr
+}
+console.log(removeDu([2,4,5,4,2,5]))
+
+// 6. how much is true
+function truecount(array){
+  let count=0
+  for (let i = 0; i < array.length; i++) {
+    if(array[i]===true){
+      count+=1
     }
-    return count
   }
-  console.log(truecount([true,false,true,false]))
-  
-  // 7.how much is false
-  function falsecount(array){
-    let count=0
-    for (let i = 0; i < array.length; i++) {
-     if(array[i]===false)
-     count+=1
-      
+  return count
+}
+console.log(truecount([true,true,false]))
+
+// 7.how much is false
+function falsecount(array){
+  let count=0
+  for (let i = 0; i < array.length; i++) {
+    if(array[i]===false){
+      count+=1
     }
-    return count
   }
-  console.log(falsecount([true,false,false,true,false,false,true]))
+  return count
+}
+console.log(falsecount([false,true,false]))
+
+// 8.check if all values are truthy
+function truthy(array){
+  for (let i = 0; i < array.length; i++) {
+    if(!array[i]===true){
+      return false
+    }
+  }
+  return true
+}
+console.log(truthy([2,2,5,-1,"Hi",""]))
+
+// 9 turn All Elements in an Array into 0
+function arrayintozero(array){
+  let newarr=[]
+  for (let i = 0; i < array.length; i++) {
+    if(!newarr.includes (array[i])){
+      newarr.push(0)
+    }
+  }
+  return newarr
+}
+console.log(arrayintozero([3,4,-1,"Hi",false,""]))
